5b5e90d28bc5505c4259a4cd4194c426ecfd5d1b
Adrian Gallus <19375915+adrian-gallus@users.noreply.github.com>
2025-05-05 19:21:01 +0200

    feat: add lazyness; extend example

--- src/example.py
+++ src/example.py
@@ -1,7 +1,53 @@
 
 from lib.signals import Signal, effect, derived
 
+
+print()
 s = Signal(1)
+
+@effect
+def logger1():
+    # NOTE this is called on each update
+    print(f"logger 1: got {s.value=}")
+
+@effect
+def logger2():
+    # NOTE this is called on each update
+    print(f"logger 2: got s={s}")
+
+@effect
+def logger3():
+    # NOTE this has no effect on update
+    print(f"logger 3: got {s=}")
+
+s.value = 2
+print()
+
+### LAZYNESS
+
+print()
+s = Signal(1)
+effect(lambda: print(f"update: {s.value=}"))
+
+p = derived(lambda: s.value % 2 == 0)
+
+@effect
+def parity():
+    # NOTE this is called on each update
+    print(f"parity of s:", "even" if p.value else "odd")
+
+s.value = 3 # no change
+s.value = 2 # change to even
+s.value = 3 # change to odd
+s.value = 5 # no change
+s.value = 6 # change to even
+print()
+
+### DECORATORS, CHAINS
+
+print()
+s = Signal(1)
+effect(lambda: print(f"update: {s.value=}"))
 
 @derived
 def t():
@@ -9,32 +55,33 @@ def t():
 
 h = derived(lambda: t.value + 1)
 
-@effect
-def status():
-    # NOTE this has no effect on update
-    print(f"status: got {s=} and {h=}")
-
+effect(lambda: print(f"got {h=}"))
 
 @effect
 def logger():
-    # NOTE this is called on each update
-    print(f"logger: got {s.value=} and h =", h)
+    # NOTE this should only emit once per update (although it has multiple dependencies)
+    print(f"logger: got {s.value=}, {h.value=}")
 
-effect(lambda: print(f"with {t=}"))
-
-print("set s = 3")
+s.value = 2
 s.value = 3
+print()
+
+### EXCEPTIONS
+
+print()
+s = Signal(1)
+effect(lambda: print(f"update: {s.value=}"))
 
 try:
     @effect
     def exception():
-        print("raising exception on t =", t)
-        raise Exception("test")
+        raise Exception(f"on s={s}")
 except:
     pass
 
 try:
-    print("set s = 4")
-    s.value = 4
+    s.value = 2
 except Exception as e:
     print("handling exception(s)", e)
+
+print()
